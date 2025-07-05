5af7977b6e99c2ceb652ae1020248d982eb402e5
shiv kumar <kumarshiv_sk@outlook.com>
2025-07-05 18:05:48 -0400

    Add project files.

---
+++ DiaryApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,49 @@
+﻿using DiaryApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DiaryApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+            
+        }
+
+        public DbSet<DiaryEntry> DiaryEntries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<DiaryEntry>().HasData(
+                new DiaryEntry
+                {
+                    Id = 1,
+                    Title = "Went Hiking",
+                    Content = "Went hiking with Joe!",
+                    Created = DateTime.Now
+                },
+                new DiaryEntry
+                {
+                    Id = 2,
+                    Title = "Went Shopping",
+                    Content = "Went shopping with Joe!",
+                    Created = DateTime.Now
+                },
+                new DiaryEntry
+                {
+                    Id = 3,
+                    Title = "Went Diving",
+                    Content = "Went diving with Joe!",
+                    Created = DateTime.Now
+                }
+
+                );
+
+
+
+        }
+
+    }
+}
---
+++ DiaryApp/Models/DiaryEntry.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DiaryApp.Models
+{
+    public class DiaryEntry     //Table name
+    {
+        //[Key]
+        public int Id { get; set; }     //If Id - it's primary key by default
+        [Required]
+        public string Title{ get; set; } = string.Empty;
+        [Required]
+        public string Content { get; set; } = string.Empty ;
+        [Required]
+        public DateTime Created { get; set; } = DateTime.Now;
+    }
+}
+
