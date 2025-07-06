9f6ba562802aea0edcf1de2916b37a9c741d033c
shiv kumar <kumarshiv_sk@outlook.com>
2025-07-06 17:17:57 -0400

    CRUD Complete

--- DiaryApp/Controllers/DiaryEntriesController.cs
+++ DiaryApp/Controllers/DiaryEntriesController.cs
@@ -43,9 +43,9 @@ namespace DiaryApp.Controllers
             return View(obj);       // page reloads and sends back the data entered by user
         }
 
+        [HttpGet]
         public IActionResult Edit(int? id)
         {
-            
             if(id == null || id == 0)
             {
                 return NotFound();
@@ -79,5 +79,31 @@ namespace DiaryApp.Controllers
             return View(obj);       // page reloads and sends back the data entered by user
         }
 
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            DiaryEntry? diaryEntry = _db.DiaryEntries.Find(id);
+
+            if (diaryEntry == null)
+            {
+                return NotFound();
+            }
+
+            return View(diaryEntry);
+        }
+
+
+        [HttpPost]
+        public IActionResult Delete(DiaryEntry obj)
+        {
+            _db.DiaryEntries.Remove(obj);   //Delete
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
